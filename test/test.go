59b25174cd0273d9714f8302b32734dd3a72ab8a
yoika <y.ikawa@yoika.net>
2016-10-12 16:23:09 +0000

    correction fo 2nd commit

--- test/test.go
+++ test/test.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
     
-    image := make([][]uint8, 32) // 行数8の行列imageを宣言。
+    image := make([][]uint8, 32) // 行数32の行列imageを宣言。
     
     for i := 0; i < 32; i++ {
-        image[i] = make([]uint8, 32) // imageの列数を8とする。
+        image[i] = make([]uint8, 32) // imageの列数を32とする。
         for j :=0; j < 32; j++ {
             image[i][j] = uint8(i*j)
             fmt.Printf("%3d ", image[i][j])
